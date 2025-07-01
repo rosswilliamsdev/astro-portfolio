e25936ef4815ee7ace9265d353649b17888aefd3
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2025-07-01 12:03:24 -0500

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
   integrations: [react()],
   site: "https://rosswilliamsdev.com",
 });
