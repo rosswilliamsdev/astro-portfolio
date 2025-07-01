f06c4363ae9a84c35b98b64984dfedc28b1942ef
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2025-07-01 12:54:47 -0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
 export default defineConfig({
   integrations: [react()],
-  site: "https://rosswilliamsdev.com",
+  site: "https://rosswilliamsdev.github.io",
+  base: "/astro-portfolio",
 });
